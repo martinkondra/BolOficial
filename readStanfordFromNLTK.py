93a04779e4d1eb82ec25f22a42450e6c76127162
Martin Kondratzky <martin.kondratzky@avature.net>
2017-07-24 09:42:46 -0300

    Fix read

--- readStanfordFromNLTK.py
+++ readStanfordFromNLTK.py
@@ -5,7 +5,7 @@ __author__ = "Martin Kondra"
 
 import pickle
 import codecs
-import snowballstemmer
+#import snowballstemmer
 
 
 data = pickle.load(open("TaggedCorpus.p", "rb")) #lista de diccionarios (sentences)
@@ -14,11 +14,12 @@ output = codecs.open("output-sentences.txt", "w", "utf-8")
 print 'Sentences with people and cargos: ', len(data), '\n'
 
 for item in data.items():
-    print item[0]
     output.write(item[0].decode('utf-8'))
     output.write('\n')
-    for i in item[1].items():
-        #print i
-        output.write(str(i))
+    for i in item[1]:
+    	for i in i.items():
+            print i
+            output.write(str(i))
+            output.write('\n')
         output.write('\n')
     output.write('\n')
